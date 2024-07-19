eec23e9b1d859fe09a6cb2b53fa30f9b366299da
Jose Llanos <jllanosgg@gmail.com>
2024-07-19 00:57:15 -0400

    agrega export

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { Audits } from "./audits";
 import { Vulnerabilities } from "./vulnerabilities"
 import { Data } from "./data"
+import { Settings } from "./settings"
 import Root from "./root";
-export { Audits, Root, Vulnerabilities, Data }
+export { Audits, Root, Vulnerabilities, Data, Settings }
