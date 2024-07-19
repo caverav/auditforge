523d59191e9bfb2010fd7b116c7df02db0aef0d0
Jose Llanos <jllanosgg@gmail.com>
2024-07-19 00:19:17 -0400

    agrega estructura inicial: router + navbar; instala paquetes de estilos/router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,41 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
 import './index.css'
+import { Root, Audits, Vulnerabilities, Data } from './routes';
+import { ErrorPage } from './error-page.tsx';
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root/>,
+    errorElement: <ErrorPage/>,
+    children: [
+      {
+        path: "/audits",
+        element: <Audits/>,
+      },
+      {
+        path: "/vulnerabilities",
+        element: <Vulnerabilities/>,
+      },
+      {
+        path: "/data",
+        element: <Data/>,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+     <RouterProvider router={router} />
   </React.StrictMode>,
 )
