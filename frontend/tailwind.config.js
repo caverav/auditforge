13b7f8fae4bc6a35cb4101f611256ae5b2a2baef
Jose Llanos <jllanosgg@gmail.com>
2024-07-19 00:29:37 -0400

    agrega content

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{js,jsx,ts,tsx}',],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
