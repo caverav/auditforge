69a2cbb27f647c1396444e1abda7002b6c2076b3
Camilo Vera Vidales <camilo.verav@sansano.usm.cl>
2024-07-20 16:00:25 -0400

    💄 style: improved (ish) 404 page

--- frontend/src/error-page.tsx
+++ frontend/src/error-page.tsx
@@ -1,22 +1,33 @@
-import {
-    useNavigate,
-    isRouteErrorResponse,
-    useRouteError,
-} from 'react-router-dom';
+import Navbar from "./components/navbar/Navbar";
+import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
 
-export const ErrorPage = () => {
-    const navigate = useNavigate();
-    const error = useRouteError() as Error;
 
-    if (!isRouteErrorResponse(error)) {
-        return null;
-    }
+export function ErrorPage() {
+  const { t } = useTranslation();
+  return (
+    <div className="bg-gray-900 h-full">
+      <Navbar />
+      <section className="home grid h-max pt-32 pb-16">
+        <div className="home__container container grid content-center gap-12 lg:max-w-5xl lg:grid-cols-2 lg:items-center m-auto">
+          <div className="home__data justify-self-center text-center lg:text-left">
+            <p className="pb-2 font-semibold text-gray-300">Error 404</p>
+            <h1 className="pb-4 text-5xl text-white font-bold lg:text-6xl">Not Found :(</h1>
+            <Link to={-1} className="inline-flex items-center justify-center rounded-full bg-gray-300 py-4 px-8 font-bold">
+              {t('goBack')}
+            </Link>
+          </div>
 
-    return (
-        <div>
-            <h1>Something went wrong 😢</h1>
-            <p>{error.data}</p>
-            <button onClick={() => navigate(-1)}>&larr; Go back</button>
+          <div className="home__img justify-self-center">
+            <img
+              src="/auditforgelogo.png"
+              className="w-64 animate-floting lg:w-[400px]"
+              alt="home image"
+            />
+            <div className="home__shadow mx-auto h-8 w-36 animate-shadow rounded-[50%] bg-gray-900/30 blur-md lg:w-64"></div>
+          </div>
         </div>
-    );
-};
+      </section>
+    </div>
+  );
+}
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,22 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+            floting: {
+              "0%,100%": { transform: "translateY(0)" },
+              "50%": { transform: "translateY(15px)" },
+            },
+            shadow: {
+              "0%,100%": { transform: "scale(1,1)" },
+              "50%": { transform: "scale(.85,.85)" },
+            },
+          },
+      animation: {
+        floting: "floting 2.5s infinite",
+        shadow: "shadow 2.5s infinite",
+      },
+    },
   },
   plugins: [],
 }
