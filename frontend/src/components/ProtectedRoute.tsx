d077a22f60dd33478674b21c85e4908dffb366ab
Jose Llanos <jllanosgg@gmail.com>
2024-07-21 15:53:32 -0400

    fix loop infinito en login

--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren, useEffect } from 'react';
+import { PropsWithChildren, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { useAuth } from './AuthProvider';
@@ -7,14 +7,26 @@ import { checktoken } from '../hooks/useAuth';
 type ProtectedRouteProps = PropsWithChildren;
 
 export default function ProtectedRoute({ children }: ProtectedRouteProps) {
+  const [loading, setLoading] = useState(true);
   const isAuth = useAuth();
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (isAuth === null || checktoken() === false) {
-      navigate('/login', { replace: true });
-    }
-  }, []);
+    const verifyAuth = async () => {
+      const tokenIsValid = await checktoken();
+      if (isAuth === false || tokenIsValid === false) {
+        navigate('/login', { replace: true });
+      } else {
+        setLoading(false);
+      }
+    };
+
+    verifyAuth();
+  }, [isAuth, navigate]);
+
+  if (loading) {
+    return <div>Loading...</div>; // TODO: Agregar loading page
+  }
 
   return children;
 }
