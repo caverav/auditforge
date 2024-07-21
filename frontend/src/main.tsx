c8e016cc5bd502dcbfdc4cc10bf21ad434d570e1
Jose Llanos <jllanosgg@gmail.com>
2024-07-20 23:32:02 -0400

    quita print

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -50,7 +50,6 @@ const router = createBrowserRouter([
 ]);
 
 checktoken().then((result) => {
-  console.log(result);
   ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <AuthProvider isSignedIn={result}>
