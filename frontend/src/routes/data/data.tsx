effb3712ec4add2a0977213949a8b53b2062bafa
Jose Llanos <jllanosgg@gmail.com>
2024-07-23 22:26:32 -0400

    agrega ejemplo de input simple

--- frontend/src/routes/data/data.tsx
+++ frontend/src/routes/data/data.tsx
@@ -1,11 +1,30 @@
+import { useState } from "react";
+import Card from "../../components/card/Card";
+import SimpleInput from "../../components/input/SimpleInput";
+
 export const Data = () => {
+  const [valor, setValor] = useState("");
 
-    return (
-        <>
+  /* const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setValor(e.target.value);
+  }; */
 
-            Datos
-        
-        </>
-    )
-
-}
+  return (
+    <div className="w-2/3 mt-8 mx-auto">
+      <Card title="Test input simple">
+        <SimpleInput
+          label="Nombre:"
+          name="nombre"
+          id="nombre"
+          type="text"
+          placeholder="Ingrese su nombre"
+          value={valor}
+          onChange={setValor}
+        />
+      </Card>
+      <Card title={valor}>
+        <div>aslkdjsajlkd</div>
+      </Card>
+    </div>
+  );
+};
