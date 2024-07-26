454447b9f4b834831a7ef36192a6c34c09ef8d85
Camilo Vera Vidales <camilo.verav@sansano.usm.cl>
2024-07-26 14:21:33 -0400

    🔥 removed settings changes

--- frontend/src/routes/settings/settings.tsx
+++ frontend/src/routes/settings/settings.tsx
@@ -1,127 +1,112 @@
-import Card from "../../components/card/Card";
-import { t } from "i18next";
-import PrimarySwitch from "../../components/switch/PrimarySwitch";
-import { useEffect, useState } from "react";
-import SelectDropdown from "../../components/dropdown/SelectDropdown";
-import { useTranslation } from "react-i18next";
-import { CheckCircleIcon, NoSymbolIcon } from "@heroicons/react/24/outline";
-import RadioGroup from "../../components/button/RadioGroup";
-import CheckboxButton from "../../components/button/CheckboxButton";
+import Card from "../../components/card/Card"
+import { t } from "i18next"
+import PrimarySwitch from "../../components/switch/PrimarySwitch"
+import { useEffect, useState } from "react"
+import SelectDropdown from "../../components/dropdown/SelectDropdown"
+import { useTranslation } from "react-i18next"
+import { CheckCircleIcon, NoSymbolIcon } from "@heroicons/react/24/outline"
 
 const languageOptions = [
-  {
-    id: 1,
-    value: "en-US",
-  },
-  {
-    id: 2,
-    value: "de-DE",
-  },
-  {
-    id: 3,
-    value: "fr-FR",
-  },
-  {
-    id: 4,
-    value: "zh-CN",
-  },
-];
-
-const RadioOptions = [
-  { id: '1', label: 'Opcion 1', value: '1' },
-  { id: '2', label: 'Opcion 2', value: '2' },
-  { id: '3', label: 'Opcion 3', value: '3', disabled: true },
-];
+    {
+        id: 1,
+        value: 'en-US'
+    },
+    {
+        id: 2,
+        value: 'de-DE'
+    },
+    {
+        id: 3,
+        value: 'fr-FR'
+    },
+    {
+        id: 4,
+        value: 'zh-CN'
+    },
+]
 
 // QUITAR ESTO
 
+
 export const Settings = () => {
-  const [enabled, setEnabled] = useState(false);
-  const [selected, setSelected] = useState({ id: 1, value: "Tom Cook" });
+    const [enabled, setEnabled] = useState(false)
+    const [selected, setSelected] = useState({ id: 1, value: 'Tom Cook' })
+    
+    // <---------------------
+    // Lógica de selector de idiomas. TODO: Moverlo a componente
 
-  // <---------------------
-  // Lógica de selector de idiomas. TODO: Moverlo a componente
+    const { i18n } = useTranslation()
 
-  const { i18n } = useTranslation();
+    var storedLanguage = localStorage.getItem("system_language");
 
-  var storedLanguage = localStorage.getItem("system_language");
+    if (!storedLanguage) {
+        storedLanguage = "en-US"
+    }
 
-  if (!storedLanguage) {
-    storedLanguage = "en-US";
-  }
+    const [language, setLanguage] = useState({
+        id: languageOptions.find(item => item.value === storedLanguage)?.id || 1,
+        value: storedLanguage
+    })
 
-  const [language, setLanguage] = useState({
-    id: languageOptions.find((item) => item.value === storedLanguage)?.id || 1,
-    value: storedLanguage,
-  });
 
-  useEffect(() => {
-    i18n.changeLanguage(language.value);
-    localStorage.setItem("system_language", language.value);
-  }, [language]);
+    useEffect(()=>{
+        i18n.changeLanguage(language.value)
+        localStorage.setItem("system_language", language.value)
+    },[language])
 
-  // --------------------->
+    // --------------------->
 
-  const handleRadioGroupChange = (value: string) => {
-    console.log('Selected value:', value);
-  };
-
-  return (
-    <div className="w-2/3 mt-8 mx-auto">
-      <Card title={t("generalSettings")}>
-        <div className="px-2">
-          <h2>{t("changeDisplayLanguage")}</h2>
-
-          <div className="mt-2 mx-2">
-            <SelectDropdown
-              title={t("changeDisplayLanguageInfo")}
-              items={languageOptions}
-              selected={language}
-              onChange={setLanguage}
-            />
-          </div>
+    return (
+        <div className="w-2/3 mt-8 mx-auto">
+            <Card
+                title={t('generalSettings')}
+            >
+                <div className="px-2">
+                    <h2>{t('changeDisplayLanguage')}</h2>
+                    
+                    <div className="mt-2 mx-2">
+                        <SelectDropdown 
+                            title={t('changeDisplayLanguageInfo')} 
+                            items={languageOptions}
+                            selected={language}
+                            onChange={setLanguage}
+                        />
+                    </div>
+                </div>
+            </Card>
+            <Card
+                title={t('generalSettings')}
+            >
+                <div className="px-2">
+                    <h2>Titulo de ejemplo</h2>
+                    <div className="mt-2 mx-2">
+                        <p>Parrafo de ejemplo. Esta card contiene dos componentes: Primary Switch, que es un switch génerico, y un menú selector tipo dropdown. 
+                            Sirve de ejemplo sobre su funcionamiento.</p>
+                    </div>
+                    <div className="mt-2 mx-2">
+                        <PrimarySwitch enabled={enabled} onChange={setEnabled}/>
+                        {
+                            enabled ? 
+                                <CheckCircleIcon className="top-8 right-8 size-12"/>
+                                    :
+                                <NoSymbolIcon className="top-8 right-8 size-12"/>
+                        }
+                        <SelectDropdown title={"Título selector"} items={[
+                            { id: 1, value: 'Tom Cook' },
+                            { id: 2, value: 'Wade Cooper' },
+                            { id: 3, value: 'Tanya Fox' },
+                            { id: 4, value: 'Arlene Mccoy' },
+                            { id: 5, value: 'Devon Webb' },
+                            ]}
+                            selected={selected}
+                            onChange={setSelected}
+                        />
+                    </div>
+                    <p>
+                        Persona seleccionada: {selected.value}
+                    </p>
+                </div>
+            </Card>
         </div>
-      </Card>
-      <Card title={t("generalSettings")}>
-        <div className="px-2">
-          <h2>Titulo de ejemplo</h2>
-          <div className="mt-2 mx-2">
-            <p>
-              Parrafo de ejemplo. Esta card contiene dos componentes: Primary
-              Switch, que es un switch génerico, y un menú selector tipo
-              dropdown. Sirve de ejemplo sobre su funcionamiento.
-            </p>
-          </div>
-          <div className="mt-2 mx-2">
-            <PrimarySwitch enabled={enabled} onChange={setEnabled} />
-            {enabled ? (
-              <CheckCircleIcon className="top-8 right-8 size-12" />
-            ) : (
-              <NoSymbolIcon className="top-8 right-8 size-12" />
-            )}
-            <SelectDropdown
-              title={"Título selector"}
-              items={[
-                { id: 1, value: "Tom Cook" },
-                { id: 2, value: "Wade Cooper" },
-                { id: 3, value: "Tanya Fox" },
-                { id: 4, value: "Arlene Mccoy" },
-                { id: 5, value: "Devon Webb" },
-              ]}
-              selected={selected}
-              onChange={setSelected}
-            />
-          </div>
-          <p>Persona seleccionada: {selected.value}</p>
-          <RadioGroup
-            name="example"
-            options={RadioOptions}
-            defaultValue="1"
-            onChange={handleRadioGroupChange}
-          />
-          <CheckboxButton text="Texto de checkbox" />
-        </div>
-      </Card>
-    </div>
-  );
-};
+    )
+}
