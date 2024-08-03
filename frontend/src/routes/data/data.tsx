932fcaaae241ec8679a3952323242dad7ce0c6f7
Jose Llanos <jllanosgg@gmail.com>
2024-08-03 17:16:46 -0400

    agrega estructura de sección data

--- frontend/src/routes/data/data.tsx
+++ frontend/src/routes/data/data.tsx
@@ -1,3 +1,29 @@
+import { useTranslation } from "react-i18next";
+import { Sidebar } from "../../components/navbar/Sidebar";
+import { Outlet } from "react-router-dom";
+
 export const Data = () => {
-  return <>"Data"</>;
+  const { t } = useTranslation();
+  const sidebarList = [
+    { name: t("collaborators"), value: "collaborators" },
+    { name: t("companies"), value: "companies" },
+    { name: t("clients"), value: "clients" },
+    { name: t("templates"), value: "templates" },
+    { name: t("customData"), value: "customData" },
+    { name: `${t("import")} / ${t("export")}`, value: "importExport" },
+  ];
+  return (
+    <>
+      <div className="flex">
+        <Sidebar
+          title={t("handleCustomData")}
+          items={sidebarList}
+          defaultItem={sidebarList[0]}
+        />
+        <div className="p-8">
+          <Outlet />
+        </div>
+      </div>
+    </>
+  );
 };
