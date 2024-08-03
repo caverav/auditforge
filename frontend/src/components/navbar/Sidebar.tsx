d85f18c1df465363288ca4ba2004877dd381d676
Jose Llanos <jllanosgg@gmail.com>
2024-08-03 17:17:06 -0400

    agrega componente sidebar

---
+++ frontend/src/components/navbar/Sidebar.tsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+/**
+ * **name**: Traducción
+ *
+ * **value**: Valor del item para hacer el `<Link/>`
+ */
+interface listItem {
+  name: string;
+  value: string;
+}
+
+interface SidebarProps {
+  items: listItem[];
+  title: string;
+  defaultItem: listItem;
+}
+
+export const Sidebar: React.FC<SidebarProps> = ({
+  items,
+  title,
+  defaultItem,
+}) => {
+  const [selected, setSelected] = useState(defaultItem.value);
+  return (
+    <div className="w-64 h-screen bg-gray-700 text-white">
+      <div className="p-4">
+        <h1 className="text-2xl font-bold">{title}</h1>
+      </div>
+      <nav>
+        <ul>
+          {items.map((item) => (
+            <Link to={item.value}>
+              <li
+                key={item.value}
+                onClick={() => setSelected(item.value)}
+                className={`p-4 hover:bg-gray-600 cursor-pointer ${item.value == selected ? "bg-gray-600" : ""}`}
+              >
+                {item.name}
+              </li>
+            </Link>
+          ))}
+        </ul>
+      </nav>
+    </div>
+  );
+};
