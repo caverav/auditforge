cb67f3819c8d71212d6387cb4392bc843ccac8f8
Jose Llanos <jllanosgg@gmail.com>
2024-08-09 15:34:42 -0400

    mejora JSDoc

--- frontend/src/hooks/useSortableTable.tsx
+++ frontend/src/hooks/useSortableTable.tsx
@@ -5,6 +5,24 @@ import { Column } from "../components/table/UITable";
  *
  * Se deben ingresar los datos de la tabla y las columnas, definidas previamente.
  *
+ * Se debe utilizar el tipo T definido en el componente que llama al hook, que defina
+ * las columnas de los datos. **EJEMPLO**:
+ * 
+ * ```
+ * type TableData = {
+    id: number;
+    name: string;
+    age: number;
+    country: string;
+    profile: string;
+  };
+ * 
+ * ```
+ * 
+ * Luego, se llama al hook:
+ * 
+ * `const [sortedData,setTableData] = useSortableTable<TableData>(data, columns);`
+ *
  * @param data Datos de la tabla.
  * @param columns Columnas de la tabla. Utiliza el type `Column`.
  * @returns Hook para mostrar los datos de la tabla y func. `setTableData` para UITable.
