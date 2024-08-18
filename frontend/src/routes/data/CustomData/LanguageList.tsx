018091910af42b09df902052551ec0f9efa9bd31
Jose Llanos <jllanosgg@gmail.com>
2024-08-18 15:27:03 -0400

    agrega componente para encapsular DnD

---
+++ frontend/src/routes/data/CustomData/LanguageList.tsx
@@ -0,0 +1,117 @@
+import React, { useEffect, useState } from "react";
+import DraggableList from "../../../components/table/DraggableTable";
+import SimpleInput from "../../../components/input/SimpleInput";
+import PrimaryButton from "../../../components/button/PrimaryButton";
+import { Bars2Icon } from "@heroicons/react/24/outline";
+
+interface LanguageItem {
+  language: string;
+  locale: string;
+}
+
+interface LanguageListProps {
+  data: LanguageItem[];
+  isDisabled: boolean;
+  onUpdateList: (data: LanguageItem[]) => void;
+}
+
+/**
+ * Este componente renderea el contenido de
+ */
+const LanguageList: React.FC<LanguageListProps> = ({
+  data,
+  isDisabled,
+  onUpdateList,
+}) => {
+  /**
+   * Estado parcial de las filas
+   * Al mover o modificar el contenido de una, se actualiza acá
+   * Se debe hacer PUT al backend si se desea guardar los cambios
+   *
+   * También se debe agregar un atributo id único a cada fila si es que no se posee.
+   */
+  const [rows, setRows] = useState<
+    { language: string; locale: string; id: string }[]
+  >(data.map((row, index) => ({ ...row, id: index.toString() })));
+
+  /**
+   * Renderiza la fila del DnD
+   *
+   * Se debe manejar el disable de los Input en esta función.
+   */
+  const renderRow = (row: LanguageItem) => (
+    <div className="grid grid-cols-1 md:grid-cols-4 border rounded-lg p-2   ">
+      {!isDisabled && <Bars2Icon className="size-4" />}
+      <SimpleInput
+        type="text"
+        value={row.language}
+        name="language"
+        placeholder="language"
+        id="language"
+        onChange={(e) => handleInputChange(row.language, "language", e)}
+      />
+      <SimpleInput
+        type="text"
+        value={row.locale}
+        name="locale"
+        placeholder="locale"
+        id="locale"
+        onChange={(e) => handleInputChange(row.locale, "locale", e)}
+      />
+      {!isDisabled && (
+        <PrimaryButton
+          color="red"
+          onClick={() => handleRemoveRow(row.language)}
+        >
+          X
+        </PrimaryButton>
+      )}
+    </div>
+  );
+
+  const handleInputChange = (
+    id: string,
+    field: keyof LanguageItem,
+    value: string | boolean
+  ) => {
+    setRows((prevRows) =>
+      prevRows.map((row) =>
+        row.language === id ? { ...row, [field]: value } : row
+      )
+    );
+  };
+
+  const handleRemoveRow = (id: string) => {
+    setRows(rows.filter((row) => row.language !== id));
+  };
+
+  /**
+   * Le "avisa" al componente padre
+   * que cambió la lista.
+   */
+  useEffect(() => {
+    if (rows !== data) {
+      /**
+       * Le quito el "id" y envío solamente el resto al componente padre
+       */
+      onUpdateList(rows.map(({ id, ...rest }) => rest));
+    }
+  }, [rows]);
+
+  /**
+   * Se debe hacer map de las rows para agregarle ID.
+   * Puede ser cualquier string ÚNICO dentro del objeto.
+   */
+  return (
+    <div>
+      <DraggableList
+        items={rows}
+        onOrderChange={setRows}
+        renderItem={renderRow}
+        isDisabled={isDisabled}
+      />
+    </div>
+  );
+};
+
+export default LanguageList;
