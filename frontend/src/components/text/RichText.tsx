157c7651b60e38d43a44d743152c1290a2b2a2e8
Camilo Vera Vidales <camilo.verav@sansano.usm.cl>
2024-08-21 00:10:33 -0400

    🐛 fix: correct code format

--- frontend/src/components/text/RichText.tsx
+++ frontend/src/components/text/RichText.tsx
@@ -1,7 +1,7 @@
-import './css/quill.snow.css';
-import ReactQuill from 'react-quill-new';
-import { Field, Label } from '@headlessui/react'
-import './css/quill-styles.css';
+import "./css/quill.snow.css";
+import ReactQuill from "react-quill-new";
+import { Field, Label } from "@headlessui/react";
+import "./css/quill-styles.css";
 
 interface RichTextEditorProps {
   label: string;
@@ -10,40 +10,62 @@ interface RichTextEditorProps {
   onChange: (content: string) => void;
 }
 
-const RichText: React.FC<RichTextEditorProps> = ({ label, value, placeholder, onChange}) => {
-    
+const RichText: React.FC<RichTextEditorProps> = ({
+  label,
+  value,
+  placeholder,
+  onChange,
+}) => {
   const modules = {
     toolbar: [
-      [{ 'header': [1, 2, 3, false] }, { 'font': [] }],
+      [{ header: [1, 2, 3, false] }, { font: [] }],
       [{ size: [] }],
-      ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-      [{ 'list': 'ordered' }, { 'list': 'bullet' },
-      { 'indent': '-1' }, { 'indent': '+1' }],
-      ['link', 'image', 'code-block'],
-      ['clean']
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
+      ],
+      ["link", "image", "code-block"],
+      ["clean"],
     ],
     clipboard: {
       matchVisual: false,
-    }
+    },
   };
-    
 
   return (
-    <div className='p-4 w-full rounded-md border-0 text-gray-900 ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'>
+    <div className="p-4 w-full rounded-md border-0 text-gray-900 ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
       <Field>
-          <Label className="block font-medium leading-6 text-gray-300">{label}</Label>
-          <ReactQuill 
-              theme="snow" 
-              value={value} 
-              onChange={(value: string) =>  onChange(value)}
-              formats={['header', 'font', 'size', 'bold', 'italic', 'underline', 'strike', 'blockquote', 'list', 'indent', 'link', 'image', 'code-block']}
-              placeholder={placeholder}
-              className='w-full overflow-auto bg-white mt-2 p-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500'
-              modules={modules} 
-          />
+        <Label className="block font-medium leading-6 text-gray-300">
+          {label}
+        </Label>
+        <ReactQuill
+          theme="snow"
+          value={value}
+          onChange={(value: string) => onChange(value)}
+          formats={[
+            "header",
+            "font",
+            "size",
+            "bold",
+            "italic",
+            "underline",
+            "strike",
+            "blockquote",
+            "list",
+            "indent",
+            "link",
+            "image",
+            "code-block",
+          ]}
+          placeholder={placeholder}
+          className="w-full overflow-auto bg-white mt-2 p-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+          modules={modules}
+        />
       </Field>
     </div>
   );
 };
 export default RichText;
-  
