0c6a809f488207c9a34a07c0adad67054ff1635b
Jose Llanos <jllanosgg@gmail.com>
2024-08-28 16:01:05 -0400

    cambia nombre prop

--- frontend/src/components/input/SimpleInput.tsx
+++ frontend/src/components/input/SimpleInput.tsx
@@ -1,16 +1,16 @@
-import { ExclamationCircleIcon } from "@heroicons/react/24/outline";
-import { ChangeEvent, useState } from "react";
+import { ExclamationCircleIcon } from '@heroicons/react/24/outline';
+import { ChangeEvent, useState } from 'react';
 
 type SimpleInputProps = {
   label?: string;
   id: string;
   name: string;
-  type: "text" | "number" | "password";
+  type: 'text' | 'number' | 'password';
   placeholder: string;
   value: string;
   onChange: (value: string) => void;
   disabled?: boolean;
-  required?: boolean;
+  requiredAlert?: boolean;
 };
 
 const SimpleInput: React.FC<SimpleInputProps> = ({
@@ -22,7 +22,7 @@ const SimpleInput: React.FC<SimpleInputProps> = ({
   value,
   onChange,
   disabled = false,
-  required = false,
+  requiredAlert = false,
 }) => {
   const [isFocused, setIsFocused] = useState(false);
 
@@ -39,7 +39,9 @@ const SimpleInput: React.FC<SimpleInputProps> = ({
       <div className="relative mt-2 rounded-md shadow-sm">
         <input
           className={`bg-gray-800 block w-full rounded-md py-1.5 p-2 placeholder:text-gray-400 sm:text-sm sm:leading-6 ${
-            required && value === "" && !isFocused ? "ring-1 ring-red-500" : ""
+            requiredAlert && value === '' && !isFocused
+              ? 'ring-1 ring-red-500'
+              : ''
           }`}
           disabled={disabled}
           id={id}
@@ -53,7 +55,7 @@ const SimpleInput: React.FC<SimpleInputProps> = ({
           type={type}
           value={value}
         />
-        {!isFocused && required && value === "" ? (
+        {!isFocused && requiredAlert && value === '' ? (
           <span className="absolute right-3 top-0 mt-2 ml-2 text-red-500">
             <ExclamationCircleIcon className="size-5" />
           </span>
