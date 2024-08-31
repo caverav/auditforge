152139642f087816658a8202605bb419c62436ba
Jose Llanos <jllanosgg@gmail.com>
2024-08-31 18:32:18 -0400

    Revert "Merge branch 'data-pareja1' into data-pareja1-tableimplementation-collabs-comps-clients-temps"
    This reverts commit b1117793c94d8a905f9ecea2be8ea842069d7402, reversing
    changes made to a6d72fde0d3d3ee1b8a6f5d9a75c0dd1cba9eabc.

--- frontend/src/services/data.ts
+++ frontend/src/services/data.ts
@@ -1,6 +1,7 @@
-const API_URL = 'https://localhost:4242/api/';
+const API_URL = "https://localhost:4242/api/";
 
 type NewCollaborator = {
+  _id?: string;
   email: string;
   firstname: string;
   lastname: string;
@@ -9,273 +10,354 @@ type NewCollaborator = {
   role: string;
   totpenabled: boolean;
   username: string;
-};
-
-type Collaborator = {
-  email: string;
-  firstname: string;
-  lastname: string;
-  password: string;
-  phone: string;
-  role: string;
-  totpenabled: boolean;
-  enabled: boolean;
-  username: string;
-  _id: string;
-};
+  enabled?: boolean;
+}
 
 type NewCompany = {
+  _id?: string;
   name: string;
   shortName: string;
   logo: string;
-};
+}
 
 type NewClient = {
-  company: string;
+  _id?: string;
   firstname: string;
   lastname: string;
   email: string;
   title: string;
   phone: string;
   cell: string;
-};
+  company: NewCompany | null;
+}
 
 type NewTemplate = {
+  _id?: string;
   name: string;
   ext: string;
   file: string;
-};
+}
 
-const networkErrorMsg = 'Network response was not ok';
-
-export const getCollaborators = async (): Promise<{
-  status: string;
-  datas: Collaborator[];
-}> => {
+export const getRoles = async (): Promise<any> => {
   try {
-    const response = await fetch(`${API_URL}users`, {
-      credentials: 'include',
+    const response = await fetch(`${API_URL}data/roles`, {
+      credentials: "include",
     }); // Incluir token
     if (!response.ok) {
-      throw new Error(networkErrorMsg);
+      throw new Error("Network response was not ok");
     }
-    return await response.json();
+    const data = await response.json();
+    return data;
   } catch (error) {
-    console.error(error);
+    console.error("Error fetching data:", error);
+    throw error;
+  }
+};
 
+export const getCollaborators = async (): Promise<any> => {
+  try {
+    const response = await fetch(`${API_URL}users`, {
+      credentials: "include",
+    }); // Incluir token
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
     throw error;
   }
 };
 
 export const createCollaborator = async (
-  collab: NewCollaborator,
-): Promise<{
-  status: string;
-  datas: string;
-}> => {
+  collab: NewCollaborator
+): Promise<any> => {
   try {
     const response = await fetch(`${API_URL}users`, {
-      method: 'POST',
-      credentials: 'include',
-      headers: { 'Content-Type': 'application/json' },
+      method: "POST",
+      credentials: "include",
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify(collab),
     }); // Incluir token
     if (!response.ok) {
-      throw new Error(networkErrorMsg);
+      throw new Error("Network response was not ok");
     }
-    return await response.json();
+    const data = await response.json();
+    return data;
   } catch (error) {
-    console.error(error);
-
+    console.error("Error fetching data:", error);
     throw error;
   }
 };
 
-export const getCompanies = async (): Promise<{
-  status: string;
-  datas: NewCompany[];
-}> => {
+export const updateCollaborator = async (
+  collaborator: NewCollaborator
+): Promise<any> => {
+  try {
+    const { _id, ...collaboratorWithoutId } = collaborator;
+
+    const response = await fetch(`${API_URL}users/${_id}`, {
+      method: "PUT",
+      credentials: "include",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(collaboratorWithoutId),
+    });
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
+};
+
+export const getCompanies = async (): Promise<any> => {
   try {
     const response = await fetch(`${API_URL}companies`, {
-      credentials: 'include',
+      credentials: "include",
     }); // Incluir token
     if (!response.ok) {
-      throw new Error(networkErrorMsg);
+      throw new Error("Network response was not ok");
     }
-    return await response.json();
+    const data = await response.json();
+    return data;
   } catch (error) {
-    console.error(error);
-
+    console.error("Error fetching data:", error);
     throw error;
   }
 };
 
-export const getCompanies = async (): Promise<{
-  status: string;
-  datas: NewCompany[];
-}> => {
+export const createCompany = async (company: NewCompany): Promise<any> => {
   try {
     const response = await fetch(`${API_URL}companies`, {
-      credentials: 'include',
-    }); // Incluir token
-    if (!response.ok) {
-      throw new Error(networkErrorMsg);
-    }
-    return await response.json();
-  } catch (error) {
-    console.error(error);
-
-    throw error;
-  }
-};
-
-export const createCompany = async (
-  company: NewCompany,
-): Promise<{
-  status: string;
-  datas: { _id: string; name: string };
-}> => {
-  try {
-    const response = await fetch(`${API_URL}companies`, {
-      method: 'POST',
-      credentials: 'include',
-      headers: { 'Content-Type': 'application/json' },
+      method: "POST",
+      credentials: "include",
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify(company),
     });
 
     if (!response.ok) {
-      throw new Error(networkErrorMsg);
+      throw new Error("Network response was not ok");
     }
-    return await response.json();
+    const data = await response.json();
+    return data;
   } catch (error) {
-    console.error(error);
-
+    console.error("Error fetching data:", error);
     throw error;
   }
 };
 
-export const getClients = async (): Promise<{
-  status: string;
-  datas: NewClient[];
-}> => {
+export const updateCompany = async (company: NewCompany): Promise<any> => {
+  try {
+    const { _id, ...companyWithoutId } = company;
+
+    const response = await fetch(`${API_URL}companies/${_id}`, {
+      method: "PUT",
+      credentials: "include",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(companyWithoutId),
+    });
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
+};
+
+export const deleteCompany = async (companyId: string): Promise<any> => {
+  try {
+    const response = await fetch(`${API_URL}companies/${companyId}`, {
+      method: "DELETE",
+      credentials: "include",
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
+};
+
+export const getClients = async (): Promise<any> => {
   try {
     const response = await fetch(`${API_URL}clients`, {
-      credentials: 'include',
+      credentials: "include",
     }); // Incluir token
     if (!response.ok) {
-      throw new Error(networkErrorMsg);
+      throw new Error("Network response was not ok");
     }
-    return await response.json();
+    const data = await response.json();
+    return data;
   } catch (error) {
-    console.error(error);
-
+    console.error("Error fetching data:", error);
     throw error;
   }
 };
 
-export const getClients = async (): Promise<{
-  status: string;
-  datas: NewClient[];
-}> => {
+export const createClient = async (client: NewClient): Promise<any> => {
   try {
     const response = await fetch(`${API_URL}clients`, {
-      credentials: 'include',
-    }); // Incluir token
-    if (!response.ok) {
-      throw new Error(networkErrorMsg);
-    }
-    return await response.json();
-  } catch (error) {
-    console.error(error);
-
-    throw error;
-  }
-};
-
-export const createClient = async (
-  client: NewClient,
-): Promise<{
-  status: string;
-  datas: { _id: string; email: string; firstname: string; lastname: string }[];
-}> => {
-  try {
-    const response = await fetch(`${API_URL}clients`, {
-      method: 'POST',
-      credentials: 'include',
-      headers: { 'Content-Type': 'application/json' },
+      method: "POST",
+      credentials: "include",
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify(client),
     });
 
     if (!response.ok) {
-      throw new Error(networkErrorMsg);
+      throw new Error("Network response was not ok");
     }
-    return await response.json();
+    const data = await response.json();
+    return data;
   } catch (error) {
-    console.error(error);
-
+    console.error("Error fetching data:", error);
     throw error;
   }
 };
 
-export const getTemplates = async (): Promise<{
-  status: string;
-  datas: { _id: string; name: string; ext: string }[];
-}> => {
+export const updateClient = async (company: NewClient): Promise<any> => {
+  try {
+    const { _id, ...clientWithoutId } = company;
+
+    const response = await fetch(`${API_URL}clients/${_id}`, {
+      method: "PUT",
+      credentials: "include",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(clientWithoutId),
+    });
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
+};
+
+export const deleteClient = async (clientId: string): Promise<any> => {
+  try {
+    const response = await fetch(`${API_URL}clients/${clientId}`, {
+      method: "DELETE",
+      credentials: "include",
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
+};
+
+export const getTemplates = async (): Promise<any> => {
   try {
     const response = await fetch(`${API_URL}templates`, {
-      credentials: 'include',
+      credentials: "include",
     }); // Incluir token
     if (!response.ok) {
-      throw new Error(networkErrorMsg);
+      throw new Error("Network response was not ok");
     }
-    return await response.json();
+    const data = await response.json();
+    return data;
   } catch (error) {
-    console.error(error);
-
+    console.error("Error fetching data:", error);
     throw error;
   }
 };
 
-export const getTemplates = async (): Promise<{
-  status: string;
-  datas: { _id: string; name: string; ext: string }[];
-}> => {
+export const createTemplate = async (template: NewTemplate): Promise<any> => {
   try {
     const response = await fetch(`${API_URL}templates`, {
-      credentials: 'include',
-    }); // Incluir token
-    if (!response.ok) {
-      throw new Error(networkErrorMsg);
-    }
-    return await response.json();
-  } catch (error) {
-    console.error(error);
-
-    throw error;
-  }
-};
-
-export const createTemplates = async (
-  template: NewTemplate,
-): Promise<{
-  status: string;
-  datas: { _id: string; name: string; ext: string }[];
-}> => {
-  try {
-    const response = await fetch(`${API_URL}templates`, {
-      method: 'POST',
-      credentials: 'include',
-      headers: { 'Content-Type': 'application/json' },
+      method: "POST",
+      credentials: "include",
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify(template),
     });
 
     if (!response.ok) {
-      throw new Error(networkErrorMsg);
+      throw new Error("Network response was not ok");
     }
-    return await response.json();
+    const data = await response.json();
+    return data;
   } catch (error) {
-    console.error(error);
+    console.error("Error fetching data:", error);
+    throw error;
+  }
+};
 
+export const updateTemplate = async (template: NewTemplate): Promise<any> => {
+  try {
+    const { _id, ...TemplateWithoutId } = template;
+
+    const response = await fetch(`${API_URL}templates/${_id}`, {
+      method: "PUT",
+      credentials: "include",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(TemplateWithoutId),
+    });
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
+};
+
+export const deleteTemplate = async (templateId: string): Promise<any> => {
+  try {
+    const response = await fetch(`${API_URL}templates/${templateId}`, {
+      method: "DELETE",
+      credentials: "include",
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
+};
+
+export const downloadTemplate = async (templateId: string): Promise<any> => {
+  try {
+    const response = await fetch(`${API_URL}templates/download/${templateId}`, {
+      credentials: "include",
+    });
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const blob = await response.blob();
+    return blob;
+  } catch (error) {
+    console.error("Error fetching file:", error);
     throw error;
   }
 };
