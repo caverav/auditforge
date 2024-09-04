7e76ec2b6a76c9746b255a5f579f565020eee8a2
Camilo Vera Vidales <camilo@fvv.cl>
2024-09-04 18:24:49 -0400

    ✏️ typo: languages to language

--- frontend/src/routes/audits/audits.tsx
+++ frontend/src/routes/audits/audits.tsx
@@ -307,7 +307,7 @@ export const Audits = () => {
               items={languages}
               onChange={setCurrentLanguage}
               selected={currentLanguage}
-              title={t('languages')}
+              title={t('language')}
             />
           ) : null}
         </Modal>
