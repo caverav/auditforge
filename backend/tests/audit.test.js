a781ab021e3d0853c2f2a1587a5fda1dc6f9db1f
Camilo Vera Vidales <camilo@fvv.cl>
2024-09-04 23:07:33 -0400

    🎨 format: format tests

--- backend/tests/audit.test.js
+++ backend/tests/audit.test.js
@@ -3,114 +3,108 @@
   1 Audit: {name: "Audit 1", language: "en", auditType: "Web"}
 */
 
-module.exports = function(request, app) {
+module.exports = function (request, app) {
   describe('Audit Suite Tests', () => {
     var userToken = '';
 
-    var audit1Id = ""
-    var audit2Id = ""
+    var audit1Id = '';
+    var audit2Id = '';
 
     beforeAll(async () => {
-      var response = await request(app).post('/api/users/token').send({username: 'admin', password: 'Admin123'})
-      userToken = response.body.datas.token
-    })
+      var response = await request(app)
+        .post('/api/users/token')
+        .send({ username: 'admin', password: 'Admin123' });
+      userToken = response.body.datas.token;
+    });
 
     describe('Audit CRUD operations', () => {
       it('Get Audits (no existing audit in db)', async () => {
-        var response = await request(app).get('/api/audits')
-          .set('Cookie', [
-            `token=JWT ${userToken}`
-          ])
-      
-        expect(response.status).toBe(200)
-        expect(response.body.datas).toHaveLength(0)
-      })
+        var response = await request(app)
+          .get('/api/audits')
+          .set('Cookie', [`token=JWT ${userToken}`]);
+
+        expect(response.status).toBe(200);
+        expect(response.body.datas).toHaveLength(0);
+      });
 
       it('Create audit with partial information', async () => {
-        var audit = {name: "Audit 1"}
-        var response = await request(app).post('/api/audits')
-          .set('Cookie', [
-            `token=JWT ${userToken}`
-          ])
-          .send(audit)
+        var audit = { name: 'Audit 1' };
+        var response = await request(app)
+          .post('/api/audits')
+          .set('Cookie', [`token=JWT ${userToken}`])
+          .send(audit);
 
-        expect(response.status).toBe(422)
-      })
+        expect(response.status).toBe(422);
+      });
 
       it('Create audit with invalid audit type', async () => {
-        var audit = {name: "Audit 1", language: "en", auditType: "Internal Test"}
-        var response = await request(app).post('/api/audits')
-          .set('Cookie', [
-            `token=JWT ${userToken}`
-          ])
-          .send(audit)
+        var audit = {
+          name: 'Audit 1',
+          language: 'en',
+          auditType: 'Internal Test',
+        };
+        var response = await request(app)
+          .post('/api/audits')
+          .set('Cookie', [`token=JWT ${userToken}`])
+          .send(audit);
 
-        expect(response.status).toBe(404)
-      })
+        expect(response.status).toBe(404);
+      });
 
       it('Create audit', async () => {
-        var audit = {name: "Audit 1", language: "en", auditType: "Web"}
-        var response = await request(app).post('/api/audits')
-          .set('Cookie', [
-            `token=JWT ${userToken}`
-          ])
-          .send(audit)
+        var audit = { name: 'Audit 1', language: 'en', auditType: 'Web' };
+        var response = await request(app)
+          .post('/api/audits')
+          .set('Cookie', [`token=JWT ${userToken}`])
+          .send(audit);
 
-        expect(response.status).toBe(201)
-        audit1Id = response.body.datas.audit._id
-      })
+        expect(response.status).toBe(201);
+        audit1Id = response.body.datas.audit._id;
+      });
 
       it('Create second audit', async () => {
-        var audit = {name: "Audit 2", language: "fr", auditType: "Web"}
-        var response = await request(app).post('/api/audits')
-          .set('Cookie', [
-            `token=JWT ${userToken}`
-          ])
-          .send(audit)
+        var audit = { name: 'Audit 2', language: 'fr', auditType: 'Web' };
+        var response = await request(app)
+          .post('/api/audits')
+          .set('Cookie', [`token=JWT ${userToken}`])
+          .send(audit);
 
-        expect(response.status).toBe(201)
-        audit2Id = response.body.datas.audit._id
-      })
+        expect(response.status).toBe(201);
+        audit2Id = response.body.datas.audit._id;
+      });
 
       it('Delete audit', async () => {
-        var response = await request(app).delete(`/api/audits/${audit2Id}`)
-          .set('Cookie', [
-            `token=JWT ${userToken}`
-          ])
-        expect(response.status).toBe(200)
+        var response = await request(app)
+          .delete(`/api/audits/${audit2Id}`)
+          .set('Cookie', [`token=JWT ${userToken}`]);
+        expect(response.status).toBe(200);
 
-        response = await request(app).get('/api/audits')
-          .set('Cookie', [
-            `token=JWT ${userToken}`
-          ])
-        expect(response.body.datas).toHaveLength(1)
-      })
+        response = await request(app)
+          .get('/api/audits')
+          .set('Cookie', [`token=JWT ${userToken}`]);
+        expect(response.body.datas).toHaveLength(1);
+      });
 
       it('Update audit general info', async () => {
         var auditGeneralInfo = {
-                "_id": audit1Id,
-                "scope":[
-                  "Scope Item 1",
-                  "Scope Item 2",
-                ]
+          _id: audit1Id,
+          scope: ['Scope Item 1', 'Scope Item 2'],
         };
 
-        var response = await request(app).put(`/api/audits/${audit1Id}/general`)
-          .set('Cookie', [
-            `token=JWT ${userToken}`
-          ])
-          .send(auditGeneralInfo)
+        var response = await request(app)
+          .put(`/api/audits/${audit1Id}/general`)
+          .set('Cookie', [`token=JWT ${userToken}`])
+          .send(auditGeneralInfo);
 
-        expect(response.status).toBe(200)
-      })
+        expect(response.status).toBe(200);
+      });
 
       it('Get audit general info', async () => {
-        var response = await request(app).get(`/api/audits/${audit1Id}/general`)
-          .set('Cookie', [
-            `token=JWT ${userToken}`
-          ])
-      
-        expect(response.status).toBe(200)
+        var response = await request(app)
+          .get(`/api/audits/${audit1Id}/general`)
+          .set('Cookie', [`token=JWT ${userToken}`]);
+
+        expect(response.status).toBe(200);
 
         expect(response.body.datas.name).toBe('Audit 1');
         expect(response.body.datas.auditType).toBe('Web');
@@ -121,7 +115,7 @@ module.exports = function(request, app) {
         expect(response.body.datas.scope).toHaveLength(2);
         expect(response.body.datas.scope[0]).toBe('Scope Item 1');
         expect(response.body.datas.scope[1]).toBe('Scope Item 2');
-      })
-    })
-  })
-}
+      });
+    });
+  });
+};
