82c3d3a8ad1f021bb12bf2562338f33d89fcdf73
Camilo Vera Vidales <camilo@fvv.cl>
2024-09-10 22:12:37 -0300

    Revert "chore: update ESLint parser options to include frontend tsconfig file"
    This reverts commit b043100fa18811874909bd6abd5dba54381ae576.

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -16,7 +16,7 @@ module.exports = {
   ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: ['./tsconfig.json', 'frontend/tsconfig.json'],
+    project: './tsconfig.json',
     sourceType: 'module',
     ecmaVersion: 'latest',
     ecmaFeatures: {
