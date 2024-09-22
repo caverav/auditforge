ea05c538e041be0b2c6747465e4e3fd3fc3d152f
Julio Amaru Cerda Villablanca <julio.cerdav@sansano.usm.cl>
2024-09-22 20:00:17 -0300

    fix: add i18 dropdown sidebar audits

--- frontend/src/routes/audits/edit/AuditRoot.tsx
+++ frontend/src/routes/audits/edit/AuditRoot.tsx
@@ -52,11 +52,19 @@ export const AuditRoot = () => {
   }, [auditId]);
 
   const sortOptions = [
-    { id: 1, value: 'CVSS Score' },
-    { id: 2, value: 'CVSS Temporal Score' },
-    { id: 3, value: 'CVSS Environmental Score' },
-    { id: 4, value: 'Priority' },
-    { id: 5, value: 'Remediation Difficulty' },
+    { id: 1, value: 'CVSS Score', label: t('cvssScore') },
+    { id: 2, value: 'CVSS Temporal Score', label: t('cvssTemporalScore') },
+    {
+      id: 3,
+      value: 'CVSS Environmental Score',
+      label: t('cvssEnvironmentalScore'),
+    },
+    { id: 4, value: 'Priority', label: t('priority') },
+    {
+      id: 5,
+      value: 'Remediation Difficulty',
+      label: t('remediationDifficulty'),
+    },
   ];
 
   const sortOrderOptions = [
