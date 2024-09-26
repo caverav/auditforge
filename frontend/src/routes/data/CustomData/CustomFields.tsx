6deb515664cb2a20228586cf9328ce762dc6c55d
Julio Amaru Cerda Villablanca <julio.cerdav@sansano.usm.cl>
2024-09-26 11:35:46 -0300

    feat: move settings custom fields to a new component

--- frontend/src/routes/data/CustomData/CustomFields.tsx
+++ frontend/src/routes/data/CustomData/CustomFields.tsx
@@ -2,13 +2,11 @@
 import { useEffect, useState } from 'react';
 import { t } from 'i18next';
 
-import PrimaryButton from '../../../components/button/PrimaryButton';
-import SimpleInput from '../../../components/input/SimpleInput';
 import SelectDropdown from '../../../components/dropdown/SelectDropdown';
-import PrimarySwitch from '../../../components/switch/PrimarySwitch';
 import { getLanguages } from '../../../services/data';
 import { getCategories } from '../../../services/vulnerabilities';
 import { OptionsCustomData } from './custom-fields/OptionsCustomData';
+import { SettingsCustomFields } from './custom-fields/SettingsCustomFields';
 
 type ListItem = {
   id: number;
@@ -64,22 +62,6 @@ export const CustomFields: React.FC = () => {
     { id: 7, label: t('space'), value: 'space', icon: 'space_bar' },
   ];
 
-  const sizes: ListItem[] = [
-    { id: 0, label: '0', value: '0' },
-    { id: 1, label: '1', value: '1' },
-    { id: 2, label: '2', value: '2' },
-    { id: 3, label: '3', value: '3' },
-    { id: 4, label: '4', value: '4' },
-    { id: 5, label: '5', value: '5' },
-    { id: 6, label: '6', value: '6' },
-    { id: 7, label: '7', value: '7' },
-    { id: 8, label: '8', value: '8' },
-    { id: 9, label: '9', value: '9' },
-    { id: 10, label: '10', value: '10' },
-    { id: 11, label: '11', value: '11' },
-    { id: 12, label: '12', value: '12' },
-  ];
-
   const optionsList = ['checkbox', 'radio', 'select', 'select-multiple'];
 
   const [displayOptionSeleted, setDisplayOptionSeleted] = useState<ListItem>(
@@ -87,10 +69,16 @@ export const CustomFields: React.FC = () => {
   );
   const [componentOptionSelected, setComponentOptionSelected] =
     useState<ListItem | null>(null);
-  const [sizeSelected, setSizeSelected] = useState<ListItem | null>(sizes[12]);
-  const [offsetSelected, setOffsetSelected] = useState<ListItem | null>(
-    sizes[0],
-  );
+  const [sizeSelected, setSizeSelected] = useState<ListItem>({
+    id: 12,
+    label: '12',
+    value: '12',
+  });
+  const [offsetSelected, setOffsetSelected] = useState<ListItem>({
+    id: 0,
+    label: '0',
+    value: '0',
+  });
 
   const [languageSelected, setLanguageSelected] = useState<ListItem | null>(
     null,
@@ -109,25 +97,6 @@ export const CustomFields: React.FC = () => {
   const [required, setRequired] = useState(false);
 
   const [optionsData, setOptionsData] = useState<OptionData[]>([]);
-
-  // TODO: Change to a proper handler
-  const handleAddOption = () => {
-    setOptionsData([
-      {
-        locale: languageSelected?.value ?? '',
-        value: addOptionField,
-      },
-      ...optionsData,
-    ]);
-  };
-
-  const handleDeleteOption = (index: number) => {
-    setOptionsData(prevOptionsData => {
-      const updatedOptionsData = [...prevOptionsData];
-      updatedOptionsData.splice(index, 1);
-      return updatedOptionsData;
-    });
-  };
 
   // Fetch
   const fetchLanguages = async () => {
@@ -205,51 +174,19 @@ export const CustomFields: React.FC = () => {
           <div className="h-11" />
         )}
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-1 gap-4 content-start">
-        {componentOptionSelected?.value !== 'space' ? (
-          <div>
-            <SimpleInput
-              id="label"
-              label={t('label')}
-              name="label"
-              onChange={setLabel}
-              placeholder={t('label')}
-              type="text"
-              value={label}
-            />
-            <SimpleInput
-              id="description"
-              label={t('description')}
-              name="description"
-              onChange={setDescription}
-              placeholder={t('description')}
-              type="text"
-              value={description}
-            />
-          </div>
-        ) : null}
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-          <SelectDropdown
-            items={sizes}
-            onChange={value => setSizeSelected(value)}
-            selected={sizeSelected}
-            title={t('size')}
-          />
-          <SelectDropdown
-            items={sizes}
-            onChange={value => setOffsetSelected(value)}
-            selected={offsetSelected}
-            title={t('offset')}
-          />
-        </div>
-        {componentOptionSelected?.value !== 'space' ? (
-          <PrimarySwitch
-            enabled={required}
-            label={t('required')}
-            onChange={setRequired}
-          />
-        ) : null}
-      </div>
+      <SettingsCustomFields
+        componentOptionSelected={componentOptionSelected}
+        description={description}
+        label={label}
+        offsetSelected={offsetSelected}
+        required={required}
+        setDescription={setDescription}
+        setLabel={setLabel}
+        setOffsetSelected={setOffsetSelected}
+        setRequired={setRequired}
+        setSizeSelected={setSizeSelected}
+        sizeSelected={sizeSelected}
+      />
       {optionsList.includes(componentOptionSelected?.value ?? '') ? (
         <OptionsCustomData
           addOptionField={addOptionField}
