50237c787e1334527d83fd5be8ab1213c06fec36
Julio Amaru Cerda Villablanca <julio.cerdav@sansano.usm.cl>
2024-10-08 10:27:59 -0300

    fix: Change filename of PopOver component

--- frontend/src/routes/data/CustomData/custom-fields/customComponents/index.tsx
+++ frontend/src/routes/data/CustomData/custom-fields/customComponents/index.tsx
@@ -1,11 +1,13 @@
 import CheckboxButtonCustom from './CheckboxButton';
 import DayPickerCustom from './DayPickerCustom';
 import MultiSelectDropdownCustom from './MultiSelectDropdownCustom';
+import { PopOverEditCustomField } from './PopOverEditCustomField';
 import SelectDropdownCustom from './SelectDropdownCustom';
 
 export {
   CheckboxButtonCustom,
   DayPickerCustom,
   MultiSelectDropdownCustom,
+  PopOverEditCustomField,
   SelectDropdownCustom,
 };
