5031357c7af8e300fb7c643c9b40f96f7385b140
Jose Llanos <jllanosgg@gmail.com>
2024-10-09 12:49:01 -0300

    style: prop order

--- frontend/src/routes/data/CustomData/Languages.tsx
+++ frontend/src/routes/data/CustomData/Languages.tsx
@@ -129,8 +129,8 @@ export const Languages: React.FC = () => {
         />
         <div>
           <PrimaryButton
-            onClick={handleAddLanguage}
             aria-label={t('addLanguage')}
+            onClick={handleAddLanguage}
           >
             +
           </PrimaryButton>
