5c7d0084740692271e8845147af78b1087c528a1
Camilo Vera Vidales <camilo@fvv.cl>
2024-10-08 22:47:44 -0300

    🚑 fix: Remove item from data array in handleRemoveRow

--- frontend/src/routes/data/CustomData/SectionList.tsx
+++ frontend/src/routes/data/CustomData/SectionList.tsx
@@ -7,6 +7,7 @@ import SimpleInput from '../../../components/input/SimpleInput';
 import DraggableList from '../../../components/table/DraggableTable';
 
 type Section = {
+  _id: string;
   name: string;
   field: string;
   icon: string;
@@ -49,6 +50,7 @@ const SectionList: React.FC<SectionListProps> = ({
 
   const handleRemoveRow = (id: string) => {
     setRows(prevRows => prevRows.filter(row => row._id !== id));
+    data = data.filter(row => row._id !== id);
   };
   /**
    * Renderiza la fila del DnD
