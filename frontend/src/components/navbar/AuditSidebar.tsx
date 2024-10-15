413b2d20b9b7e17fbb6abc891bee8807025909c9
Camilo Vera Vidales <camilo@fvv.cl>
2024-10-15 17:12:41 -0300

    🔧 chore: Add useParams import in AuditSidebar.tsx

--- frontend/src/components/navbar/AuditSidebar.tsx
+++ frontend/src/components/navbar/AuditSidebar.tsx
@@ -2,7 +2,7 @@ import clsx from 'clsx';
 import { t } from 'i18next';
 import { ChevronDown, ChevronUp, Users } from 'lucide-react';
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 import DefaultRadioGroup from '../button/DefaultRadioGroup';
 import DropdownButton, { ListItem } from '../button/DropdownButton';
