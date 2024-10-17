4f132884366a09e59547d5be807fde778ce3c0c3
Camilo Vera Vidales <camilo@fvv.cl>
2024-10-16 21:57:47 -0300

    🚑 fix: Adjust layout in register page for better alignment and spacing

--- frontend/src/routes/register.tsx
+++ frontend/src/routes/register.tsx
@@ -36,56 +36,54 @@ export const Register = () => {
     [register],
   );
   return (
-    <>
-      <section className="bg-gray-800 h-screen flex flex-col md:flex-row justify-center space-y-10 md:space-y-0 md:space-x-16 items-center my-2 mx-5 md:mx-0 md:my-0">
-        <div className="md:w-1/3 max-w-sm">
-          <img alt="Logo" src="/logo.svg" />
-        </div>
-        <div className="md:w-1/3 max-w-sm text-black">
-          <h1 className="text-center text-2xl font-bold">
-            {t('registerFirstUser')}
-          </h1>
-          <form className="mt-4" onSubmit={handleSubmit}>
-            <input
-              className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4"
-              id="username"
-              placeholder={t('username')}
-              type="text"
-            />
-            <input
-              className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4"
-              id="firstname"
-              placeholder={t('firstname')}
-              type="text"
-            />
-            <input
-              className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4"
-              id="lastname"
-              placeholder={t('lastname')}
-              type="text"
-            />
-            <input
-              className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4"
-              id="password"
-              placeholder={t('password')}
-              type="password"
-            />
-            <input
-              className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4"
-              id="confirmPassword"
-              placeholder={t('confirmPassword')}
-              type="password"
-            />
+    <section className="bg-gray-800 h-screen flex flex-col md:flex-row justify-center space-y-10 md:space-y-0 md:space-x-16 items-center my-2 mx-5 md:mx-0 md:my-0">
+      <div className="md:w-1/3 max-w-sm">
+        <img alt="Logo" src="/logo.svg" />
+      </div>
+      <div className="md:w-1/3 max-w-sm text-black">
+        <h1 className="text-center text-2xl font-bold">
+          {t('registerFirstUser')}
+        </h1>
+        <form className="mt-4" onSubmit={handleSubmit}>
+          <input
+            className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4"
+            id="username"
+            placeholder={t('username')}
+            type="text"
+          />
+          <input
+            className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4"
+            id="firstname"
+            placeholder={t('firstname')}
+            type="text"
+          />
+          <input
+            className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4"
+            id="lastname"
+            placeholder={t('lastname')}
+            type="text"
+          />
+          <input
+            className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4"
+            id="password"
+            placeholder={t('password')}
+            type="password"
+          />
+          <input
+            className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4"
+            id="confirmPassword"
+            placeholder={t('confirmPassword')}
+            type="password"
+          />
 
-            <button
-              className="mt-4 bg-black hover:bg-gray-100 hover:text-black px-4 py-2 text-white uppercase rounded text-xs tracking-wider"
-              type="submit"
-            >
-              {t('btn.create')}
-            </button>
-          </form>
-        </div>
-      </section>
+          <button
+            className="mt-4 bg-black hover:bg-gray-100 hover:text-black px-4 py-2 text-white uppercase rounded text-xs tracking-wider"
+            type="submit"
+          >
+            {t('btn.create')}
+          </button>
+        </form>
+      </div>
       <Toaster
         toastOptions={{
           classNames: {
@@ -96,6 +94,6 @@ export const Register = () => {
           },
         }}
       />
-    </>
+    </section>
   );
 };
