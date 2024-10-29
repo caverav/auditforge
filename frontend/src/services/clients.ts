3bfcd0ace126eeaaa0d90f93c02e9d8950187ab0
Ricardo <ricardo.olalquiaga@sansano.usm.cl>
2024-10-29 19:13:35 -0300

    fix: removed unused variable

--- frontend/src/services/clients.ts
+++ frontend/src/services/clients.ts
@@ -1,5 +1,4 @@
 import { getAudits } from './audits';
-const API_URL = `${import.meta.env.VITE_API_URL}/api/`;
 
 export const getAuditsByClientName = async (clientName: string) => {
   // get all audits and filter by clientName
