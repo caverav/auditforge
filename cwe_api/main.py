aa302502a9ce9d5669ed426a3a4177c744dc7202
Sebastián Alvarado <sebastian.alvarador@usm.cl>
2024-10-30 09:27:17 -0300

    feat: added check checksum endpoint

--- cwe_api/main.py
+++ cwe_api/main.py
@@ -1,4 +1,7 @@
 import ssl
+import hashlib
+import requests
+from pathlib import Path
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
@@ -15,6 +18,26 @@ app.add_middleware(
     allow_methods=["GET", "POST", "PUT", "DELETE"],  # Métodos HTTP permitidos
     allow_headers=["*"],  # Cabeceras permitidas
 )
+
+LOCAL_FILE_PATH = Path("/app/modelo_cwe.zip")
+REMOTE_FILE_URL = "https://drive.usercontent.google.com/download?id=1OtRNObv-Il2B5nDnpzMSGj_yBJAlskuS&export=download&confirm="
+BYTE_RANGE = (0, 10485759)  # 10 MB
+
+def calculate_local_checksum(file_path, byte_limit):
+    sha256_hash = hashlib.sha256()
+    with open(file_path, "rb") as f:
+        chunk = f.read(byte_limit)
+        sha256_hash.update(chunk)
+    return sha256_hash.hexdigest()
+
+def calculate_remote_checksum(url, byte_range):
+    headers = {'Range': f'bytes={byte_range[0]}-{byte_range[1]}'}
+    response = requests.get(url, headers=headers, stream=True)
+
+    sha256_hash = hashlib.sha256()
+    for chunk in response.iter_content(chunk_size=8192):
+        sha256_hash.update(chunk)
+    return sha256_hash.hexdigest()
 
 class VulnerabilityRequest(BaseModel):
     vuln: str
@@ -39,6 +62,14 @@ async def read_root():
     )
     result = inferencer(example_vuln)
     return {"example_vuln": example_vuln, "result": result}
+
+@app.get("/check_cwe_update")
+async def check_cwe_update():
+    local_checksum = calculate_local_checksum(LOCAL_FILE_PATH, byte_limit=BYTE_RANGE[1] + 1)
+    remote_checksum = calculate_remote_checksum(REMOTE_FILE_URL, BYTE_RANGE)
+
+    match = local_checksum == remote_checksum
+    return {"checksum_match": match}
 
 if __name__ == "__main__":
     import uvicorn
