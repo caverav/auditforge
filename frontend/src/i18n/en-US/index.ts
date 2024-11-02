816b310ac7a19d9675e898bd9ec37f365b75efb1
Julio Amaru Cerda Villablanca <julio.cerdav@sansano.usm.cl>
2024-11-02 16:26:25 -0300

    feat: add new translation for error at deleting finding

--- frontend/src/i18n/en-US/index.ts
+++ frontend/src/i18n/en-US/index.ts
@@ -202,6 +202,7 @@ export default {
     passwordsDontMatch: 'Passwords do not match',
     errorGeneratingPdf: 'Failed exporting audit to pdf',
     errorSavingAuditCustomSections: 'Failed saving the audit custom sections',
+    failedDeleteFinding: 'Failed to delete the finding',
   },
   cvss: {
     title: 'CVSS v3.1 Base Score',
