d7243a9565320d01b927479381773b10b3adc68b
Camilo Vera Vidales <camilo@fvv.cl>
2024-11-03 20:42:11 -0300

    ♻️ refactor: update most common label in CWECloud component

--- frontend/src/components/charts/CWECloud.tsx
+++ frontend/src/components/charts/CWECloud.tsx
@@ -34,7 +34,9 @@ export const CWECloud: React.FC<Props> = ({ items, mostCommon }) => {
   }
   return (
     <div className="bg-gray-900 rounded-lg h-[300px] overflow-hidden">
-      <p className="text-sm text-gray-400">Most common: {mostCommon}</p>
+      <p className="text-sm text-gray-400">
+        {t('label.mostCommon')} {mostCommon}
+      </p>
       <div className="flex flex-wrap gap-2 justify-center items-center">
         <Wordcloud
           font="Impact"
