41533ed174d65e78b3fb7005afcabbfa966edd2b
Jose Llanos <jllanosgg@gmail.com>
2024-11-03 01:13:12 -0300

    feat: CWE model update endpoints

--- frontend/src/services/settings.ts
+++ frontend/src/services/settings.ts
@@ -1,4 +1,6 @@
 const API_URL = `${import.meta.env.VITE_API_URL}/api/`;
+const checkUpdateUrl = `${import.meta.env.VITE_API_URL}/api/check-cwe-update`;
+const updateCWEModelUrl = `${import.meta.env.VITE_API_URL}/api/update-cwe-model`;
 
 export const getSettings = async (): Promise<any> => {
   try {
@@ -13,5 +15,45 @@ export const getSettings = async (): Promise<any> => {
     console.error('Error fetching data:', error);
 
     throw error;
+  }
+};
+
+/**
+ * Retorna `true` si no hay actualización del modelo de CWE,
+ * `false` de lo contrario.
+ */
+export const checkUpdateCWE = async (): Promise<boolean> => {
+  try {
+    const response = await fetch(checkUpdateUrl, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include',
+    });
+
+    const data = await response.json();
+
+    return data.checksum_match;
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+};
+
+export const updateCWEModel = async (): Promise<object> => {
+  try {
+    const response = await fetch(updateCWEModelUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include',
+    });
+
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+    return { status: 'error', error };
   }
 };
