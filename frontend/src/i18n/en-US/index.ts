39d0e64f602569412746102273bb7dac94573a5d
Camilo Vera Vidales <camilo@fvv.cl>
2024-11-04 16:44:16 -0300

    ✏️ fix(i18n): correct typo in 'occurrences' key in en-US translations

--- frontend/src/i18n/en-US/index.ts
+++ frontend/src/i18n/en-US/index.ts
@@ -744,5 +744,5 @@ export default {
   averageCvss: 'Average CVSS',
   averageRemediationPriority: 'Average Remediation Priority',
   clientSelect: 'Select your client',
-  ocurrences: 'Ocurrences',
+  occurrences: 'Occurrences',
 };
